0b77867522c30cf753f3e725041e862bfcf5f178
BlairABlake <k111ryousuke@gmail.com>
2023-07-02 08:36:23 +0900

    fix foreach method

--- datastructures.js
+++ datastructures.js
@@ -1,11 +1,15 @@
-class Pair {
+class Vec2D {
     constructor(x, y) {
         this.x = x
         this.y = y
     }
+
+    add(other) {
+        return new Vec2D(this.x + other.x, this.y + other.y)
+    }
 }
 
-class PriorityQueueElement extends Pair {
+class PriorityQueueElement extends Vec2D {
     get priority() { return this.y }
 }
 
@@ -23,11 +27,87 @@ class PriorityQueue {
     insert() {}
     remove() {}
     update() {}
-    size() {}
+    forEach() {}
+
+    size() {
+        return this.xs.length
+    }
+
+    lastIndex() {
+        return this.size() - 1
+    }
+
+    empty() {
+        return this.size() == 0
+    }
+
+    /**
+     * 
+     * @param {Function} func
+     * @returns reference to this object
+     */
+    map(func) {
+        if(this.empty()) return this
+        
+        return this.xs.map(e => func(e.x, e.y))
+    }
+
+    /**
+     * 
+     * @param {Function} func
+     * @returns 
+     */
+    flatMap(func) {
+        if(this.empty()) return this
+        
+        return this.xs.flatMap(e => func(e.x, e.y))
+    }
 }
 
 
 class SortedList extends PriorityQueue {
+    constructor(reverse=false) {
+        super()
+        this.reverse = reverse
+    }
+
+    top() {
+        return this.xs.pop()
+    }
+
+    peek() {
+        return this.xs[0]
+    }
+
+    insert(element, priority) {
+        if(this.reverse) priority *= -1
+        let index = this.xs.findIndex(v => v.priority >= priority)
+        let e = new PriorityQueueElement(element, priority)
+        if (index < 0) {
+            this.xs.push(e)
+        } else if(index == 0) {
+            [e].push(...this.xs)
+        } else {
+            let xs = this.xs.slice(index-1)
+            xs.push(e)
+            xs.push(...this.xs.slice(index, this.size()))
+            this.xs = xs
+        }
+    }
+
+    remove(element) {
+        let index = this.xs.indexOf(element)
+        this.xs.splice(index, 1)
+    }
+
+    update(element, newPriority) {
+        this.remove(element)
+        this.insert(element, newPriority)
+    }
+
+    forEach(func) {
+        this.xs.forEach(e => func(e.x, e.y))
+    }
 
 }
 
@@ -41,17 +121,29 @@ class Heap extends PriorityQueue {
         if (this.size() == 0) this._emptyError("top()")
 
         let last = this.xs.pop()
-        if (this.size() == 0) return last.x
+        if (this.size() == 0) return last
 
         let first = this.xs[0]
         this.xs[0] = last
         this._pushDown(0)
 
-        return first.x
+        return first
     }
 
     peek() {
-        return this.xs[0].x
+        return this.xs[0]
+    }
+
+    size() {
+        return this.xs.length
+    }
+
+    lastIndex() {
+        return this.size() - 1
+    }
+
+    empty() {
+        return this.size() == 0
     }
 
     insert(element, priority) {
@@ -75,39 +167,28 @@ class Heap extends PriorityQueue {
         let index = this.xs.findIndex(element)
         if (index < 0) return 
 
-        let oldPriority = this.xs[index].priority
+        let oldPriority = this.xs[index].y
         if(oldPriority < newPriority) this._bubbleUp(index)
         else this._pushDown(index)
     }
 
-    size() {
-        return this.xs.length
-    }
-
-    lastIndex() {
-        return this.size() - 1
-    }
-
-    empty() {
-        return this.size() == 0
-    }
-
-    foreach(func) {
-        if(this.empty()) return
-
-        let t = clone(this)
+    forEach(func) {
+        let t = new Heap()
+        t.xs = this.xs.slice(0)
+        if(t.empty()) return
 
         do {
             let e = t.top()
-            func(e)
+            func(e.x, e.y)
         } while (!t.empty());
     }
+
 
     _bubbleUp(index) {
         const loop = () => {
             let parentIndex = Math.ceil(index/2) - 1
             if(parentIndex < 0) return
-            if(this.xs[index].priority > this.xs[parentIndex].priority) {
+            if(this.xs[index].y > this.xs[parentIndex].y) {
                 [this.xs[index], this.xs[parentIndex]] = [this.xs[parentIndex], this.xs[index]]
                 index = parentIndex
                 loop()
@@ -125,7 +206,7 @@ class Heap extends PriorityQueue {
             else if(children.length == 1) childIndex = this._leftIndex(index)
             else if(children.length == 2) childIndex = this._highestPriorityChildIndex(index)
 
-            if(this.xs[index].priority < this.xs[childIndex].priority) {
+            if(this.xs[index].y < this.xs[childIndex].y) {
                 [this.xs[index], this.xs[childIndex]] = [this.xs[childIndex], this.xs[index]]
                 index = childIndex
                 loop()
@@ -149,7 +230,7 @@ class Heap extends PriorityQueue {
     }
 
     _highestPriorityChildIndex(index) {
-        return this.xs[this._leftIndex(index)].priority >= this.xs[this._rightIndex(index)].priority ? this._leftIndex(index) : this._rightIndex(index)
+        return this.xs[this._leftIndex(index)].y >= this.xs[this._rightIndex(index)].y ? this._leftIndex(index) : this._rightIndex(index)
     }
 
     _emptyError(operationName) {

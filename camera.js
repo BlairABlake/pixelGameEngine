3b5250656519d357aa4d4a2a3064ee52d00c0eb9
BlairABlake <k111ryousuke@gmail.com>
2023-07-07 19:25:27 +0900

    feat: add camera.js

---
+++ camera.js
@@ -0,0 +1,12 @@
+/**
+ * Camera is essentially a container of a viewport
+ */
+class Camera {
+    /**
+     * 
+     * @param {Vec2D} center
+     */
+    constructor(center) {
+        this.center = center
+    }
+}
